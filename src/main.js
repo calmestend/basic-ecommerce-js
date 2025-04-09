8911fa7b9bf111e5c6bd2fc8bfeead399fb1722e
Barac Fabregat <barac.fabregat.gallegos@tuta.com>
2025-04-08 22:20:23 -0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hello Vite!</h1>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
